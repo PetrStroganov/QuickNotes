a864e5ccd2acac3aa255d047a7140b565e112650
PetrStroganov <kojlovpetr@gmail.com>
2025-03-15 19:17:04 +0300

    обновлен логин

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import bcrypt
 from flask import Flask, render_template, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from forms import RegistrationForm, LoginForm
 from models import db, User
 from config import Config
@@ -11,6 +10,7 @@ app.config.from_object(Config)
 db.init_app(app)
 
 login_manager = LoginManager(app)
+login_manager.init_app(app)
 login_manager.login_view = 'login'
 
 with app.app_context():
@@ -19,7 +19,7 @@ with app.app_context():
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    return db.session.get(User, int(user_id))
 
 
 @app.route('/')
@@ -44,7 +44,7 @@ def register():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
+        user = db.session.scalar(db.select(User).filter_by(username=form.username.data))
         if user and bcrypt.checkpw(form.password.data.encode('utf-8'), user.password.encode('utf-8')):
             login_user(user)
             return redirect(url_for('notes'))

941d1447c77ed1e1858535791088bd50269daa87
Mike2838378282 <121633280+Mike2838378282@users.noreply.github.com>
2025-05-08 16:19:22 +0300

    Update app.py, исправил приватные заметки, добавил вкладку все заметки

--- app.py
+++ app.py
@@ -17,22 +17,29 @@ login_manager.login_view = 'login'
 with app.app_context():
     db.create_all()
 
-
 @login_manager.user_loader
 def load_user(user_id):
     return db.session.get(User, int(user_id))
-
 
 @app.route('/')
 @login_required
 def index():
     tag = request.args.get('tag')
-    query = db.select(Note).filter_by(user_id=current_user.id, is_private=False)
+    query = db.select(Note).filter_by(user_id=current_user.id)
     if tag:
-        query = query.filter(Note.tags.like(f'%{tag}%'))
+        query = query.filter(Note.tags.ilike(f"%{tag}%"))
     notes = db.session.scalars(query.order_by(Note.created_at.desc())).all()
-    return render_template('notes.html', notes=notes)
+    return render_template('notes.html', notes=notes, current_tag=tag, tab='mine')
 
+@app.route('/all_notes')
+@login_required
+def all_notes():
+    tag = request.args.get('tag')
+    query = db.select(Note).filter(Note.is_private == False)
+    if tag:
+        query = query.filter(Note.tags.ilike(f"%{tag}%"))
+    notes = db.session.scalars(query.order_by(Note.created_at.desc())).all()
+    return render_template('notes.html', notes=notes, current_tag=tag, tab='all')
 
 @app.route('/add_note', methods=['GET', 'POST'])
 @login_required
@@ -57,7 +64,6 @@ def add_note():
             flash('Ошибка при создании заметки', 'error')
     return render_template('add_note.html', form=form)
 
-
 @app.route('/note/<int:note_id>')
 @login_required
 def view_note(note_id):
@@ -65,7 +71,6 @@ def view_note(note_id):
     if not note or note.user_id != current_user.id:
         abort(404)
     return render_template('note_detail.html', note=note)
-
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -86,7 +91,6 @@ def register():
             flash('Произошла ошибка при регистрации', 'error')
     return render_template('register.html', form=form)
 
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -100,13 +104,11 @@ def login():
             flash('Неверный логин или пароль', 'error')
     return render_template('login.html', form=form)
 
-
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('login'))
-
 
 if __name__ == "__main__":
     app.run(port=8080, host="127.0.0.1")
